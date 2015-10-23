8cd59a39d107b6838607ba28fd5c74be2a595394
unknown <hamishmclennan@outlook.com>
2015-10-23 14:51:03 +1300

    update2

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -1,8 +1,7 @@
 <html>
 <head>
 	<title>CTrak</title>
-	<link rel="shortcut icon" href="/CTrak/Images/Ball-Favicon.png">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/landing.css') }}">
+
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" >
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/landing.css') }}">
