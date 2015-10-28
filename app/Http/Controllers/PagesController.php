ee332efd0f5e9a4ecc494c436154c17b1475f957
unknown <hamishmclennan@outlook.com>
2015-10-29 09:20:11 +1300

    fixed errors

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -60,7 +60,7 @@ class PagesController extends Controller
 
         if(!Auth::attempt($request->only(['username','password'])))
         {
-            dd('failed')
+            dd('failed');
         }
     }
 /*
