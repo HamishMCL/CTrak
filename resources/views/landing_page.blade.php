841e6457547453fcfdde260a8087f266600ae828
unknown <hamishmclennan@outlook.com>
2015-10-29 19:12:51 +1300

    test quick deploy

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>CTrak</title>
+	<title>CTraks</title>
 
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" >
