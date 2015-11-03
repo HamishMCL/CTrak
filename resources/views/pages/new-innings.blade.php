6f6006239c27b10289db2d7f85fcc2457b41ff4f
unknown <hamishmclennan@outlook.com>
2015-11-03 22:57:32 +1300

    beta

--- resources/views/pages/new-innings.blade.php
+++ resources/views/pages/new-innings.blade.php
@@ -21,11 +21,11 @@
 			
 			 
 			 	<h4>6's</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="sixes" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="sixes" type="number" class="form-control input-md">
 			
 			     
 			 	 <h4>4's</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="fours" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="fours" type="number" class="form-control input-md">
 			
 			  	 <h4>Out</h4> 
 			  	<div class="radio">
@@ -41,19 +41,19 @@
 
 				<h3 style="text-decoration: underline;;">Bowling</h3>
 				 	<h4>Wickets</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="wickets" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="wickets" type="number" class="form-control input-md">
 			  	 	<h4>Runs conceded</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="runs_conceded" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="runs_conceded" type="number" class="form-control input-md">
 			  	 	<h4>Overs</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="overs" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="overs" type="number" class="form-control input-md">
 			  	 	<h4>Maidens</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="maidens" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="maidens" type="number" class="form-control input-md">
 
 			  	<h3 style="text-decoration: underline;">Feilding</h3>
 			  	  	 	<h4>Catches</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="catches" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="catches" type="number" class="form-control input-md">
 			  	  	 	<h4>Run outs</h4> 
-			  	<input id="textinput" style="padding:0px;margin:0px;" name="run_outs" type="text" class="form-control input-md">
+			  	<input id="textinput" style="padding:0px;margin:0px;" name="run_outs" type="number" class="form-control input-md">
 
 
 			   <button  id="signin_btn"type="submit"  style="margin-top: 5px; width: 260px;"class="btn btn-primary"><p>Submit</p></button>
